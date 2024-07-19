43b78d63a10fd32d681e520dce4b2b316a47091c
Tiago Santos <tiagossantostsdev@gmail.com>
2024-07-20 00:54:25 +0100

    Some update in cors again.

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ dbConnection();
 //app uses
 app.use(express.json());
 app.use(cors({
-    origin: "https://server-todolist-bay.vercel.app"
+    origin: "https://todolist-tiagosantos.netlify.app"
 }));
 
 //app uses routes
