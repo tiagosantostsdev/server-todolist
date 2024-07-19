76ebcab06a86f73319dda2dee5921aab785a87a7
Tiago Santos <tiagossantostsdev@gmail.com>
2024-07-20 00:44:36 +0100

    I fixed the cors error.

--- src/index.ts
+++ src/index.ts
@@ -10,26 +10,22 @@ import { taskRoutes } from "./routers/taskRoute";
 dotenv.config({ path: "./src/.env" });
 
 //Instace express
-try {
-  const app = express();
+const app = express();
 
-  //database connection
-  dbConnection();
+//database connection
+dbConnection();
 
-  //app uses
-  app.use(express.json());
-  app.use(cors());
+//app uses
+app.use(express.json());
+app.use(cors());
 
-  //app uses routes
-  app.use("/user", userRoute);
-  app.use("/auth", loginRoute);
-  app.use("/tasks", taskRoutes);
+//app uses routes
+app.use("/user", userRoute);
+app.use("/auth", loginRoute);
+app.use("/tasks", taskRoutes);
 
-  //sever port
-  const PORT: number = Number(process.env.PORT || 2002);
-  app.listen(PORT, () => {
-    console.log({ message: `Server running... at ${PORT} port` });
-  });
-} catch (error) {
-  console.error(error);
-}
+//sever port
+const PORT: number = Number(process.env.PORT || 2002);
+app.listen(PORT, () => {
+  console.log({ message: `Server running... at ${PORT} port` });
+});
