02635450157650eac86e49209436edd6cf25c247
Tiago Santos <tiagossantostsdev@gmail.com>
2024-07-20 14:06:01 +0100

    Adicionei novamente o async await na conexão do banco de dados.

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-export const dbConnection = () => {
+export const dbConnection = async () => {
   const mongodbURI: string = String(process.env.MONGODB_URI);
-  mongoose
+  await mongoose
     .connect(mongodbURI)
     .then(() => console.log("MongoDB connected successfully via Mongoose"))
     .catch((err) => {
