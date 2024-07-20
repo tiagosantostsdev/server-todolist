98371db690126373487b37d1967b1b7917ef0e46
Tiago Santos <tiagossantostsdev@gmail.com>
2024-07-20 17:36:43 +0100

    Fixed the send redefinition password.

--- src/config/redefinePassword.ts
+++ src/config/redefinePassword.ts
@@ -1,16 +1,9 @@
 import nodemailer from "nodemailer";
 
 export const RedefinePasswordEmail = (email: string, code: string) => {
-  const contentHtml = `
-  <html>
-  <body style="width:100%;">
-      <h2>Digite o código de verificação para redefinir a senha de usuário.</h2>
-      <p style="font-size: 1.5rem; text-align: center; border-radius: .5rem; letter-spacing: .6rem; font-weight: bold; padding: .2rem; background-color: rgb(128, 141, 126); width:50%">${code}</p>
-  </body>
-  </html>`;
 
-  const user: string = String(process.env.USER_EMAIL);
-  const pass: string = String(process.env.USER_PASS);
+  const user = "devsangola5@gmail.com"
+  const pass = "hryy upwd vpew ixcw"
 
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -27,7 +20,14 @@ export const RedefinePasswordEmail = (email: string, code: string) => {
     {
       from: user,
       subject: "Redefinição de Senha de usuário",
-      html: contentHtml,
+      to: email,
+      html: `
+  <html>
+  <body style="width:100%;">
+      <h2>Digite o código de verificação para redefinir a senha de usuário.</h2>
+      <p style="font-size: 1.5rem; text-align: center; border-radius: .5rem; letter-spacing: .6rem; font-weight: bold; padding: .2rem; background-color: rgb(128, 141, 126); width:50%">${code}</p>
+  </body>
+  </html>`,
     },
     (error, info) => {
       if (error) {
